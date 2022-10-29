d1b50a7f11f5e62363938709dced20ad0b59b9df
zibranhasan <zibranhasansourab@gmail.com>
2022-10-29 18:33:49 +0600

    added a new component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import "./Cart.css"
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total price:</p>
+            <p>Total shipping:</p>
+            <p>Tax:</p>
+            <h5>Grand Total:</h5>
         </div>
     );
 };
